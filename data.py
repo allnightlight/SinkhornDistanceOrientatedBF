f5bd7cde71e0cfd68d7652fb05b911d5f053ba4c
Shouta Ukai <shouta.ukai@gmail.com>
2019-06-09 20:47:32 +0900

    Add test data
    Test data sampled from Random walk is added to test data.

--- data.py
+++ data.py
@@ -1,6 +1,38 @@
 from scipy.integrate import odeint
 import numpy as np
 
+
+class GeneratorFromRandomWalk():
+    def __init__(self, T = 200, dt = 0.1, test_ratio = 0.1, TimeConstant = 2**4):
+
+        t = np.arange(0, T, dt)
+        x = np.random.randn(1)
+        alpha = 1 - 1/TimeConstant
+        beta = np.sqrt(1 - alpha**2)
+        X = []
+        for _ in t:
+            x = alpha * x + beta * np.random.randn(1)
+            X.append(x)
+
+        self.X = np.stack(X, axis=0).astype(np.float32)
+        self.Ntest = int(len(t) * test_ratio)
+        self.Ntrain = len(t) - self.Ntest
+        self.Nx = 1
+
+    def batch(self, Nbatch, N):
+        idx = np.random.randint(low=0, high=self.Ntrain-N, size=(1,Nbatch)) + \
+            np.arange(N).reshape(-1,1) # (N, Nbatch)
+        Xbatch = self.X[idx,:] # (N, Nbatch, 1)
+        return Xbatch
+
+    def test(self, N):
+        Nbatch = self.Ntest - N
+        idx = np.random.randint(low=self.Ntrain, high=self.Ntrain+self.Ntest-N, 
+            size=(1,Nbatch)) + np.arange(N).reshape(-1,1) # (N, Nbatch)
+        Xbatch = self.X[idx,:] # (N, Nbatch, 1)
+        return Xbatch, Nbatch
+
+
 class GeneratorFromSinCurve():
     def __init__(self, T = 200, dt = 0.1, test_ratio = 0.1):
 
